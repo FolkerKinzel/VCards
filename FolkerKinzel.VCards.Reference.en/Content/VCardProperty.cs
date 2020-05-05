bbd6b5277f2fa10abc8c4a3bfc65ea341918eae7
Folker Kinzel <fkinzel@freenet.de>
2020-05-05 14:23:56 +0200

    english project reference added

---
+++ FolkerKinzel.VCards.Reference.en/Content/VCardProperty.cs
@@ -0,0 +1,8 @@
+﻿public abstract class VCardProperty<T>
+{
+    public string? Group { get; set; }
+
+    public ParameterSection Parameters { get; }
+
+    public virtual T Value { get; protected set; }
+}
