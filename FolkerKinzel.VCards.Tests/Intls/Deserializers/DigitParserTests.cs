9a8df3daf609e13b7fd8731479e7a4127b870616
Folker Kinzel <fkinzel@freenet.de>
2021-01-08 10:22:01 +0100

    DigitParser

---
+++ FolkerKinzel.VCards.Tests/Intls/Deserializers/DigitParserTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FolkerKinzel.VCards.Intls.Deserializers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FolkerKinzel.VCards.Intls.Deserializers.Tests
+{
+    [TestClass()]
+    public class DigitParserTests
+    {
+        [TestMethod()]
+        public void ParseTest()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                string s = i.ToString();
+
+                Assert.AreEqual(i, DigitParser.Parse(s[0]));
+            }
+        }
+    }
+}
---
+++ FolkerKinzel.VCards/Intls/Deserializers/DigitParser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace FolkerKinzel.VCards.Intls.Deserializers
+{
+    internal static class DigitParser
+    {
+#if !NET40
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+        internal static int Parse(char c) => (int)c - 48;
+    }
+}
