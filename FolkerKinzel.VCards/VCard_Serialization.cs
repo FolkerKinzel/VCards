b84f5ac0bcb860a553623505818f5c77e9165603
Folker Kinzel <fkinzel@freenet.de>
2021-01-17 19:37:06 +0100

    Serialize Stream

--- FolkerKinzel.VCards/VCard_Serialization.cs
+++ FolkerKinzel.VCards/VCard_Serialization.cs
@@ -20,16 +20,12 @@ namespace FolkerKinzel.VCards
         /// <summary>
         /// Speichert eine Liste von <see cref="VCard"/>-Objekten in eine gemeinsame VCF-Datei.
         /// </summary>
-        /// <param name="fileName">Der Dateipfad.</param>
-        /// <param name="vcards">Die zu speichernden <see cref="VCard"/>-Objekte. Die Auflistung darf leer sein oder <c>null</c>-Werte
+        /// <param name="fileName">Der Dateipfad. Wenn die Datei existiert, wird sie überschrieben.</param>
+        /// <param name="vCardList">Die zu speichernden <see cref="VCard"/>-Objekte. Die Auflistung darf leer sein oder <c>null</c>-Werte
         /// enthalten. Wenn die Auflistung kein <see cref="VCard"/>-Objekt enthält, wird keine Datei geschrieben.</param>
         /// <param name="version">Die vCard-Version, in die die Datei serialisiert wird.</param>
         /// <param name="options">Optionen für das Schreiben der VCF-Datei. Die Flags können
         /// kombiniert werden.</param>
-        /// <exception cref="ArgumentNullException"><paramref name="fileName"/> oder <paramref name="vcards"/>
-        /// ist <c>null</c>.</exception>
-        /// <exception cref="ArgumentException"><paramref name="fileName"/> ist kein gültiger Dateipfad.</exception>
-        /// <exception cref="IOException">Die Datei konnte nicht geschrieben werden.</exception>
         /// <remarks>
         /// <note type="caution">
         /// Obwohl die Methode selbst threadsafe ist, sind es die an die Methode übergebenen 
@@ -37,36 +33,37 @@ namespace FolkerKinzel.VCards
         /// <see cref="VCard"/>-Objekte während der Ausführung dieser Methode!
         /// </note>
         /// <para>Die Methode serialisiert möglicherweise auch dann mehrere
-        /// vCards, wenn <paramref name="vcards"/> nur ein <see cref="VCard"/>-Objekt enthielt - nämlich dann,
+        /// vCards, wenn <paramref name="vCardList"/> nur ein <see cref="VCard"/>-Objekt enthielt - nämlich dann,
         /// wenn dieses <see cref="VCard"/>-Objekt in den Properties <see cref="VCard.Members"/> oder <see cref="VCard.Relations"/> 
         /// weitere <see cref="VCard"/>-Objekte referenziert hat.</para>
-        /// 
         /// </remarks>
+        /// <exception cref="ArgumentNullException"><paramref name="fileName"/> oder <paramref name="vCardList"/>
+        /// ist <c>null</c>.</exception>
+        /// <exception cref="ArgumentException"><paramref name="fileName"/> ist kein gültiger Dateipfad.</exception>
+        /// <exception cref="IOException">Die Datei konnte nicht geschrieben werden.</exception>
 #if !NET40
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
         public static void Save(
             string fileName,
-            List<VCard?> vcards,
+            List<VCard?> vCardList,
             VCdVersion version = VCdVersion.V3_0,
             VcfOptions options = VcfOptions.Default)
         {
-            if (vcards is null)
+            if (vCardList is null)
             {
-                throw new ArgumentNullException(nameof(vcards));
+                throw new ArgumentNullException(nameof(vCardList));
             }
 
-            if (!vcards.Any(x => x != null))
+            if (!vCardList.Any(x => x != null))
             {
                 return;
             }
 
             try
             {
-                // UTF-8 muss ohne BOM geschrieben werden, da sonst nicht lesbar
-                // (vCard 2.1 kann UTF-8 verwenden, da nur ASCII-Zeichen geschrieben werden)
-                using var writer = new StreamWriter(fileName, false, new UTF8Encoding(false));
-                Serialize(writer, vcards, version, options);
+                using var stream = new FileStream(fileName, FileMode.Create, FileAccess.Write, FileShare.None);
+                Serialize(stream, vCardList, version, options);
             }
             catch (ArgumentNullException)
             {
@@ -100,7 +97,7 @@ namespace FolkerKinzel.VCards
 
 
         /// <summary>
-        /// Serialisiert eine Liste von <see cref="VCard"/>-Objekten mit einem <see cref="TextWriter"/>.
+        /// Serialisiert eine Liste von <see cref="VCard"/>-Objekten in einen <see cref="Stream"/>.
         /// </summary>
         /// <remarks><para>Die Methode serialisiert möglicherweise auch dann mehrere
         /// vCards, wenn <paramref name="vCardList"/> nur ein <see cref="VCard"/>-Objekt enthielt - nämlich dann,
@@ -112,30 +109,38 @@ namespace FolkerKinzel.VCards
         /// <see cref="VCard"/>-Objekte während der Ausführung dieser Methode!
         /// </note>
         /// </remarks>
-        /// <param name="writer">Ein <see cref="TextWriter"/>, mit dem die serialisierten <see cref="VCard"/>-Objekte geschrieben werden.</param>
+        /// <param name="stream">Ein <see cref="Stream"/>, in den die serialisierten <see cref="VCard"/>-Objekte geschrieben werden. <paramref name="stream"/>
+        /// wird von der Methode geschlossen.</param>
         /// <param name="vCardList">Die zu serialisierenden <see cref="VCard"/>-Objekte.</param>
         /// <param name="version">Die vCard-Version, in die die Datei serialisiert wird.</param>
         /// <param name="options">Optionen für das Schreiben der VCF-Datei. Die Flags können
         /// kombiniert werden.</param>
-        /// <exception cref="ArgumentNullException"><paramref name="vCardList"/> ist <c>null</c>.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="stream"/> oder <paramref name="vCardList"/> ist <c>null</c>.</exception>
+        /// <exception cref="ArgumentException"><paramref name="stream"/> unterstützt keine Schreibvorgänge.</exception>
         /// <exception cref="IOException">E/A-Fehler.</exception>
-        /// <exception cref="ObjectDisposedException">Die Ressourcen von <paramref name="writer"/> sind bereits freigegeben.</exception>
+        /// <exception cref="ObjectDisposedException"><paramref name="stream"/> war bereits geschlossen.</exception>
 #if !NET40
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
-        public static void Serialize(TextWriter writer, List<VCard?> vCardList, VCdVersion version, VcfOptions options = VcfOptions.Default)
+        public static void Serialize(Stream stream, List<VCard?> vCardList, VCdVersion version, VcfOptions options = VcfOptions.Default)
         {
             DebugWriter.WriteMethodHeader($"{nameof(VCard)}.{nameof(Serialize)}({nameof(TextWriter)}, List<{nameof(VCard)}>, {nameof(VCdVersion)}, {nameof(VcfOptions)}");
 
-            if (writer is null)
+            if (stream is null)
             {
-                throw new ArgumentNullException(nameof(writer));
+                throw new ArgumentNullException(nameof(stream));
             }
+
+            // UTF-8 muss ohne BOM geschrieben werden, da sonst nicht lesbar
+            // (vCard 2.1 kann UTF-8 verwenden, da nur ASCII-Zeichen geschrieben werden)
+            using var writer = new StreamWriter(stream, new UTF8Encoding(false));
 
             if (vCardList is null)
             {
                 throw new ArgumentNullException(nameof(vCardList));
             }
+
+            
 
             while (vCardList.Remove(null)) { }
 
@@ -249,7 +254,8 @@ namespace FolkerKinzel.VCards
         /// <summary>
         /// Serialisiert die <see cref="VCard"/> mit einem <see cref="TextWriter"/>.
         /// </summary>
-        /// <param name="writer">Ein <see cref="TextWriter"/>, mit dem die <see cref="VCard"/> serialisiert wird.</param>
+        /// <param name="stream">Der <see cref="Stream"/>, in den die <see cref="VCard"/> serialisiert wird. <paramref name="stream"/>
+        /// wird von der Methode geschlossen.</param>
         /// <param name="version">Die vCard-Version, in die serialisiert wird.</param>
         /// <param name="options">Optionen für das Serialisieren. Die Flags können
         /// kombiniert werden.</param>
@@ -263,11 +269,15 @@ namespace FolkerKinzel.VCards
         /// sich aus Performancegründen die Verwendung der statischen Methoden der Klasse <see cref="VCard"/>.
         /// </para>
         /// </remarks>
+        /// <exception cref="ArgumentNullException"><paramref name="stream"/> ist <c>null</c>.</exception>
+        /// <exception cref="ArgumentException"><paramref name="stream"/> unterstützt keine Schreibvorgänge.</exception>
+        /// <exception cref="IOException">E/A-Fehler.</exception>
+        /// <exception cref="ObjectDisposedException"><paramref name="stream"/> war bereits geschlossen.</exception>
 #if !NET40
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
-        public void Serialize(TextWriter writer, VCdVersion version, VcfOptions options = VcfOptions.Default)
-            => VCard.Serialize(writer, new List<VCard?> { this }, version, options);
+        public void Serialize(Stream stream, VCdVersion version, VcfOptions options = VcfOptions.Default)
+            => VCard.Serialize(stream, new List<VCard?> { this }, version, options);
 
 
         /// <summary>
@@ -288,9 +298,11 @@ namespace FolkerKinzel.VCards
             // kein Inlining, da schon VCard.Serialize ge-inlined ist und die Methode in Tests
             // häufig aufgerufen wird
 
-            using var stringWriter = new StringWriter();
-            VCard.Serialize(stringWriter, new List<VCard?> { this }, version, options);
-            return stringWriter.ToString();
+            using var stream = new MemoryStream();
+
+            VCard.Serialize(stream, new List<VCard?> { this }, version, options);
+
+            return Encoding.UTF8.GetString(stream.ToArray());
         }
 
 
