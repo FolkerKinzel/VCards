f29028f0a02a5dc8c43aa37f17109e349b713f49
Folker Kinzel <fkinzel@freenet.de>
2021-02-07 23:13:57 +0100

    Refactoring

--- FolkerKinzel.VCards/Models/PropertyParts/Name.cs
+++ FolkerKinzel.VCards/Models/PropertyParts/Name.cs
@@ -1,4 +1,5 @@
-﻿using FolkerKinzel.VCards.Intls.Extensions;
+﻿using FolkerKinzel.VCards.Intls.Converters;
+using FolkerKinzel.VCards.Intls.Extensions;
 using FolkerKinzel.VCards.Intls.Serializers;
 using FolkerKinzel.VCards.Models.Enums;
 using System;
@@ -16,14 +17,18 @@ namespace FolkerKinzel.VCards.Models.PropertyParts
     /// </summary>
     public sealed class Name
     {
-        private readonly ReadOnlyCollection<string>[] _data;
-
         private const int MAX_COUNT = 5;
+
         private const int LAST_NAME = 0;
         private const int FIRST_NAME = 1;
         private const int MIDDLE_NAME = 2;
         private const int PREFIX = 3;
         private const int SUFFIX = 4;
+        private readonly ReadOnlyCollection<string> _lastName;
+        private readonly ReadOnlyCollection<string> _firstName;
+        private readonly ReadOnlyCollection<string> _middleName;
+        private readonly ReadOnlyCollection<string> _prefix;
+        private readonly ReadOnlyCollection<string> _suffix;
 
         /// <summary>
         /// Initialisiert ein neues <see cref="Name"/>-Objekt.
@@ -34,39 +39,37 @@ namespace FolkerKinzel.VCards.Models.PropertyParts
         /// <param name="prefix">Namenspräfix (z.B. "Prof. Dr.")</param>
         /// <param name="suffix">Namenssuffix (z.B. "jr.")</param>
         internal Name(
-            IEnumerable<string?>? lastName = null,
-            IEnumerable<string?>? firstName = null,
-            IEnumerable<string?>? middleName = null,
-            IEnumerable<string?>? prefix = null,
-            IEnumerable<string?>? suffix = null)
+            ReadOnlyCollection<string> lastName,
+            ReadOnlyCollection<string> firstName,
+            ReadOnlyCollection<string> middleName,
+            ReadOnlyCollection<string> prefix,
+            ReadOnlyCollection<string> suffix)
         {
-#if NET40
-            string[] empty = VCard.EmptyStringArray;
-#else       
-            string[] empty = Array.Empty<string>();
-#endif
-            var arr = new ReadOnlyCollection<string>[]
-            {
-#nullable disable
-                new ReadOnlyCollection<string>(lastName?.Where(x => !string.IsNullOrWhiteSpace(x)).ToArray() ?? empty),
-                    new ReadOnlyCollection<string>(firstName?.Where(x => !string.IsNullOrWhiteSpace(x)).ToArray() ?? empty),
-                    new ReadOnlyCollection<string>(middleName?.Where(x => !string.IsNullOrWhiteSpace(x)).ToArray() ?? empty),
-                    new ReadOnlyCollection<string>(prefix?.Where(x => !string.IsNullOrWhiteSpace(x)).ToArray() ?? empty),
-                    new ReadOnlyCollection<string>(suffix?.Where(x => !string.IsNullOrWhiteSpace(x)).ToArray() ?? empty)
-#nullable restore
-            };
-
-            _data = arr;
+            _lastName = lastName;
+            _firstName = firstName;
+            _middleName = middleName;
+            _prefix = prefix;
+            _suffix = suffix;
         }
+
+        internal Name()
+        {
+            _lastName =
+            _firstName =
+            _middleName =
+            _prefix =
+            _suffix = ReadOnlyCollectionConverter.Empty();
+        }
+
 
         internal Name(string vCardValue, StringBuilder builder, VCdVersion version)
         {
             Debug.Assert(vCardValue != null);
 
-            var listList = vCardValue
+            List<List<string>?>? listList = vCardValue
                 .SplitValueString(';')
                 .Select(x => x.SplitValueString(',', StringSplitOptions.RemoveEmptyEntries))
-                .ToList();
+                .ToList()!;
 
             for (int i = 0; i < listList.Count; i++)
             {
@@ -78,7 +81,7 @@ namespace FolkerKinzel.VCards.Models.PropertyParts
                 {
                     _ = builder.Clear();
                     _ = builder.Append(currentList[j]);
-                    _ = builder.UnMask(version).Trim().RemoveQuotes();
+                    _ = builder.UnMask(version); //.Trim().RemoveQuotes();
 
                     if (builder.Length != 0)
                     {
@@ -98,38 +101,75 @@ namespace FolkerKinzel.VCards.Models.PropertyParts
                 listList.Add(new List<string>());
             }
 
-            _data = listList.Select(x => new ReadOnlyCollection<string>(x)).ToArray();
+            _firstName = ReadOnlyCollectionConverter.ToReadOnlyCollection(listList[FIRST_NAME]);
+            _lastName = ReadOnlyCollectionConverter.ToReadOnlyCollection(listList[LAST_NAME]);
+            _middleName = ReadOnlyCollectionConverter.ToReadOnlyCollection(listList[MIDDLE_NAME]);
+            _prefix = ReadOnlyCollectionConverter.ToReadOnlyCollection(listList[PREFIX]);
+            _suffix = ReadOnlyCollectionConverter.ToReadOnlyCollection(listList[SUFFIX]);
         }
 
         /// <summary>
         /// Nachname (nie <c>null</c>)
         /// </summary>
-        public ReadOnlyCollection<string> LastName => _data[LAST_NAME];
+        public ReadOnlyCollection<string> LastName => _lastName;
 
         /// <summary>
         /// Vorname (nie <c>null</c>)
         /// </summary>
-        public ReadOnlyCollection<string> FirstName => _data[FIRST_NAME];
+        public ReadOnlyCollection<string> FirstName => _firstName;
 
         /// <summary>
         /// zweiter Vorname (nie <c>null</c>)
         /// </summary>
-        public ReadOnlyCollection<string> MiddleName => _data[MIDDLE_NAME];
+        public ReadOnlyCollection<string> MiddleName => _middleName;
 
         /// <summary>
         /// Namenspräfix (z.B. "Prof. Dr.") (nie <c>null</c>)
         /// </summary>
-        public ReadOnlyCollection<string> Prefix => _data[PREFIX];
+        public ReadOnlyCollection<string> Prefix => _prefix;
 
         /// <summary>
         /// Namenssuffix (z.B. "jr.") (nie <c>null</c>)
         /// </summary>
-        public ReadOnlyCollection<string> Suffix => _data[SUFFIX];
+        public ReadOnlyCollection<string> Suffix => _suffix;
+
 
         /// <summary>
         /// <c>true</c>, wenn das <see cref="Name"/>-Objekt keine verwertbaren Daten enthält.
         /// </summary>
-        public bool IsEmpty => !_data.Any(x => x.Count != 0);
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0046:In bedingten Ausdruck konvertieren", Justification = "<Ausstehend>")]
+        public bool IsEmpty
+        {
+            get
+            {
+                if (_lastName.Count != 0)
+                {
+                    return false;
+                }
+
+                if (_firstName.Count != 0)
+                {
+                    return false;
+                }
+
+                if (_middleName.Count != 0)
+                {
+                    return false;
+                }
+
+                if (_prefix.Count != 0)
+                {
+                    return false;
+                }
+
+                if (_suffix.Count != 0)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+        }
 
 
         internal void AppendVCardString(VcfSerializer serializer)
@@ -139,13 +179,21 @@ namespace FolkerKinzel.VCards.Models.PropertyParts
 
             char joinChar = serializer.Version < VCdVersion.V4_0 ? ' ' : ',';
 
-            for (int i = 0; i < _data.Length - 1; i++)
-            {
-                AppendProperty(_data[i]);
-                _ = builder.Append(';');
-            }
+            AppendProperty(_lastName);
+            _ = builder.Append(';');
 
-            AppendProperty(_data[_data.Length - 1]);
+            AppendProperty(_firstName);
+            _ = builder.Append(';');
+
+            AppendProperty(_middleName);
+            _ = builder.Append(';');
+
+            AppendProperty(_prefix);
+            _ = builder.Append(';');
+
+            AppendProperty(_suffix);
+
+            ///////////////////////////////////
 
             void AppendProperty(IList<string> strings)
             {
@@ -165,15 +213,32 @@ namespace FolkerKinzel.VCards.Models.PropertyParts
             }
         }
 
-
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0046:In bedingten Ausdruck konvertieren", Justification = "<Ausstehend>")]
         internal bool NeedsToBeQpEncoded()
         {
-            for (int i = 0; i < _data.Length; i++)
+            if (_lastName.Any(x => x.NeedsToBeQpEncoded()))
             {
-                if (_data[i].Any(s => s.NeedsToBeQpEncoded()))
-                {
-                    return true;
-                }
+                return true;
+            }
+
+            if (_firstName.Any(x => x.NeedsToBeQpEncoded()))
+            {
+                return true;
+            }
+
+            if (_middleName.Any(x => x.NeedsToBeQpEncoded()))
+            {
+                return true;
+            }
+
+            if (_prefix.Any(x => x.NeedsToBeQpEncoded()))
+            {
+                return true;
+            }
+
+            if (_suffix.Any(x => x.NeedsToBeQpEncoded()))
+            {
+                return true;
             }
 
             return false;
@@ -189,24 +254,71 @@ namespace FolkerKinzel.VCards.Models.PropertyParts
             var worker = new StringBuilder();
             var dic = new List<Tuple<string, string>>();
 
-            for (int i = 0; i < _data.Length; i++)
+            for (int i = 0; i < MAX_COUNT; i++)
             {
-                string? s = BuildProperty(_data[i]);
-
-                if (s is null)
+                switch (i)
                 {
-                    continue;
+                    case LAST_NAME:
+                        {
+                            string? s = BuildProperty(_lastName);
+
+                            if (s is null)
+                            {
+                                continue;
+                            }
+
+                            dic.Add(new Tuple<string, string>(nameof(LastName), s));
+                            break;
+                        }
+                    case FIRST_NAME:
+                        {
+                            string? s = BuildProperty(_firstName);
+
+                            if (s is null)
+                            {
+                                continue;
+                            }
+
+                            dic.Add(new Tuple<string, string>(nameof(FirstName), s));
+                            break;
+                        }
+                    case MIDDLE_NAME:
+                        {
+                            string? s = BuildProperty(_middleName);
+
+                            if (s is null)
+                            {
+                                continue;
+                            }
+
+                            dic.Add(new Tuple<string, string>(nameof(MiddleName), s));
+                            break;
+                        }
+                    case PREFIX:
+                        {
+                            string? s = BuildProperty(_prefix);
+
+                            if (s is null)
+                            {
+                                continue;
+                            }
+
+                            dic.Add(new Tuple<string, string>(nameof(Prefix), s));
+                            break;
+                        }
+                    case SUFFIX:
+                        {
+                            string? s = BuildProperty(_suffix);
+
+                            if (s is null)
+                            {
+                                continue;
+                            }
+
+                            dic.Add(new Tuple<string, string>(nameof(Suffix), s));
+                            break;
+                        }
                 }
-
-                dic.Add(new Tuple<string, string>(i switch
-                {
-                    LAST_NAME => nameof(LastName),
-                    FIRST_NAME => nameof(FirstName),
-                    MIDDLE_NAME => nameof(MiddleName),
-                    PREFIX => nameof(Prefix),
-                    SUFFIX => nameof(Suffix),
-                    _ => "Not Implemented"
-                }, s));
             }
 
             if (dic.Count == 0)
@@ -228,6 +340,8 @@ namespace FolkerKinzel.VCards.Models.PropertyParts
 
             worker.Length -= Environment.NewLine.Length;
             return worker.ToString();
+
+            //////////////////////////////////////////////////
 
             string? BuildProperty(IList<string> strings)
             {
