602f5b121a13c2898145ab8d86fa12f226e0e53c
Folker Kinzel <fkinzel@freenet.de>
2021-02-08 17:14:30 +0100

    Unit tests

---
+++ FolkerKinzel.VCards.Tests/Intls/Deserializers/ValueSplitterTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FolkerKinzel.VCards.Intls.Deserializers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FolkerKinzel.VCards.Tests.Intls.Deserializers
+{
+    [TestClass]
+    public class ValueSplitterTests
+    {
+        [DataTestMethod]
+        [DataRow(null, ',', StringSplitOptions.None, 0)]
+        [DataRow(null, ';', StringSplitOptions.None, 0)]
+        [DataRow(null, ',', StringSplitOptions.RemoveEmptyEntries, 0)]
+        [DataRow(null, ';', StringSplitOptions.RemoveEmptyEntries, 0)]
+        [DataRow("", ',', StringSplitOptions.None, 1)]
+        [DataRow("", ';', StringSplitOptions.None, 1)]
+        [DataRow("", ',', StringSplitOptions.RemoveEmptyEntries, 0)]
+        [DataRow("", ';', StringSplitOptions.RemoveEmptyEntries, 0)]
+        [DataRow(",", ',', StringSplitOptions.None, 2)]
+        [DataRow(",", ';', StringSplitOptions.None, 1)]
+        [DataRow(",", ',', StringSplitOptions.RemoveEmptyEntries, 0)]
+        [DataRow(",", ';', StringSplitOptions.RemoveEmptyEntries, 1)]
+        [DataRow(";", ',', StringSplitOptions.None, 1)]
+        [DataRow(";", ';', StringSplitOptions.None, 2)]
+        [DataRow(";", ',', StringSplitOptions.RemoveEmptyEntries, 1)]
+        [DataRow(";", ';', StringSplitOptions.RemoveEmptyEntries, 0)]
+        [DataRow(@"\,", ',', StringSplitOptions.None, 1)]
+        [DataRow(@"\,", ';', StringSplitOptions.None, 1)]
+        [DataRow(@"\,", ',', StringSplitOptions.RemoveEmptyEntries, 1)]
+        [DataRow(@"\,", ';', StringSplitOptions.RemoveEmptyEntries, 1)]
+        [DataRow(@"\;", ',', StringSplitOptions.None, 1)]
+        [DataRow(@"\;", ';', StringSplitOptions.None, 1)]
+        [DataRow(@"\;", ',', StringSplitOptions.RemoveEmptyEntries, 1)]
+        [DataRow(@"\;", ';', StringSplitOptions.RemoveEmptyEntries, 1)]
+        public void GetEnumeratorTest1(string? valueString, char splitChar, StringSplitOptions options, int expectedParts)
+        {
+            int count = 0;
+
+            foreach (var s in new ValueSplitter(valueString, splitChar, options))
+            {
+                Assert.IsNotNull(s);
+                count++;
+            }
+
+            Assert.AreEqual(expectedParts, count);
+        }
+
+
+        [DataTestMethod]
+        [DataRow(@"Bun\,go,Bon\;go;Banga", ',', new string[] { @"Bun\,go", @"Bon\;go;Banga" })]
+        [DataRow(@"Bun\,go,Bon\;go;Banga", ';', new string[] { @"Bun\,go,Bon\;go", "Banga" })]
+        public void GetEnumeratorTest2(string? valueString, char splitChar, string[] expected)
+        {
+            var arr = new ValueSplitter(valueString, splitChar).ToArray();
+            CollectionAssert.AreEqual(arr, expected, StringComparer.Ordinal);
+        }
+
+    }
+}
---
+++ FolkerKinzel.VCards/Intls/Deserializers/ValueSplitter.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FolkerKinzel.VCards.Intls.Deserializers
+{
+    internal readonly struct ValueSplitter : IEnumerable<string>
+    {
+        private readonly string? _valueString;
+        private readonly char _splitChar;
+        private readonly StringSplitOptions _options;
+
+        public ValueSplitter(string? valueString, char splitChar, StringSplitOptions options = StringSplitOptions.None)
+        {
+            this._valueString = valueString;
+            this._splitChar = splitChar;
+            this._options = options;
+        }
+
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            if (_valueString is null)
+            {
+                yield break;
+            }
+
+            int i = 0;
+
+            while (i <= _valueString.Length)
+            {
+                int splitIndex = GetNextSplitIndex(i);
+
+                if (splitIndex == i)
+                {
+                    if (_options == StringSplitOptions.None)
+                    {
+                        yield return string.Empty;
+                    }
+                }
+                else if (_options != StringSplitOptions.RemoveEmptyEntries || ContainsData(0, _valueString.Length))
+                {
+                    int length = splitIndex - i;
+
+                    yield return length == _valueString.Length 
+                                    ? _valueString
+                                    : length == 0 
+                                        ? string.Empty 
+                                        : _valueString.Substring(i, length);
+                }
+
+                i = splitIndex + 1;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        private int GetNextSplitIndex(int startIndex)
+        {
+            string s = _valueString!;
+            bool masked = false;
+
+            for (int i = startIndex; i < s.Length; i++)
+            {
+                if (masked)
+                {
+                    masked = false;
+                    continue;
+                }
+
+                char c = s[i];
+
+                if (c == _splitChar)
+                {
+                    return i;
+                }
+
+                if (c == '\\')
+                {
+                    masked = true;
+                }
+
+            }//for
+
+            return s.Length;
+        }
+
+        private bool ContainsData(int startIndex, int length)
+        {
+            string s = _valueString!;
+            for (int i = startIndex; i < length; i++)
+            {
+                if (char.IsWhiteSpace(s[i]))
+                {
+                    continue;
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+
+    }
+}
