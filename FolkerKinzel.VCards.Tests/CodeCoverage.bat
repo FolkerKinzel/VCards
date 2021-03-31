79eb25ee5ced19b3d6c9bcbe5916c8f8e9b6cd4b
Folker Kinzel <fkinzel@freenet.de>
2021-03-31 11:45:05 +0200

    Code coverage

---
+++ FolkerKinzel.VCards.Tests/CodeCoverage.bat
@@ -0,0 +1,2 @@
+dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutput=./reports/ /p:ExcludeByAttribute=\"Obsolete,GeneratedCode,CompilerGenerated\" /p:SkipAutoProps=true /p:DoesNotReturnAttribute="DoesNotReturnAttribute"
+reportgenerator -reports:./reports/coverage.net5.0.cobertura.xml -targetdir:./reports/html/
