c3479ccb83f74f8bc62db0a4663429e259746d1c
Folker Kinzel <fkinzel@freenet.de>
2021-11-12 12:05:43 +0100

    Unit tests

---
+++ src/FolkerKinzel.VCards.Tests/Intls/Converters/VCdVersionConverterTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FolkerKinzel.VCards.Intls.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FolkerKinzel.VCards.Models.Enums;
+
+namespace FolkerKinzel.VCards.Intls.Converters.Tests
+{
+    [TestClass()]
+    public class VCdVersionConverterTests
+    {
+        [DataTestMethod()]
+        [DataRow(null, VCdVersion.V2_1)]
+        [DataRow("bla", VCdVersion.V2_1)]
+        [DataRow("", VCdVersion.V2_1)]
+        [DataRow("2.1", VCdVersion.V2_1)]
+        [DataRow("3.0", VCdVersion.V3_0)]
+        [DataRow("4.0", VCdVersion.V4_0)]
+        public void ParseTest(string? input, VCdVersion expected) => Assert.AreEqual(expected, VCdVersionConverter.Parse(input));
+    }
+}
--- src/FolkerKinzel.VCards/Intls/Converters/VCdVersionConverter.cs
+++ src/FolkerKinzel.VCards/Intls/Converters/VCdVersionConverter.cs
@@ -6,19 +6,14 @@ namespace FolkerKinzel.VCards.Intls.Converters
     {
         internal static VCdVersion Parse(string? value)
         {
-            try
-            {
-                return (value?[0]) switch
+            return string.IsNullOrEmpty(value)
+                ? VCdVersion.V2_1
+                : value[0] switch
                 {
                     '4' => VCdVersion.V4_0,
                     '3' => VCdVersion.V3_0,
                     _ => VCdVersion.V2_1
                 };
-            }
-            catch
-            {
-                return VCdVersion.V2_1;
-            }
         }
     }
 }
