c6b5ec0751d8418cca089c6ccae7ec6f54652d3f
Folker Kinzel <fkinzel@freenet.de>
2021-12-24 18:45:59 +0100

    Removed dead code

--- src/FolkerKinzel.VCards/Intls/Extensions/CharExtension.cs
+++
@@ -1,9 +0,0 @@
-﻿#if NET40
-namespace FolkerKinzel.VCards.Intls.Extensions
-{
-    internal static class CharExtension
-    {
-        internal static bool IsDecimalDigit(this char c) => 47u < c && 58u > c;
-    }
-}
-#endif
