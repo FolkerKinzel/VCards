b4a2e180ff57eb417c9b5b6b70e6e3383c2ccd37
Folker Kinzel <fkinzel@freenet.de>
2021-12-24 18:51:01 +0100

    Examples C# 10

--- src/Examples/Program.cs
+++ src/Examples/Program.cs
@@ -1,34 +1,28 @@
-﻿using System;
-using System.Globalization;
-using System.IO;
-using System.Threading;
-using FolkerKinzel.VCards.Models.Enums;
-using FolkerKinzel.VCards.Models.PropertyParts;
+﻿using System.Globalization;
 
-namespace Examples
+namespace Examples;
+
+internal class Program
 {
-    internal class Program
+    private static void Main()
     {
-        private static void Main()
+        Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+        Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
+
+
+        string directoryPath = Path.GetFullPath("TestFiles");
+
+        if (Directory.Exists(directoryPath))
         {
-            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
-            Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
-
-
-            string directoryPath = Path.GetFullPath("TestFiles");
-
-            if (Directory.Exists(directoryPath))
-            {
-                Directory.Delete(directoryPath, true);
-            }
-
-            _ = Directory.CreateDirectory(directoryPath);
-
-
-            //WhatsAppDemo1.IntegrateWhatsAppNumberUsingIMPP();
-            //WhatsAppDemo2.UsingTheWhatsAppType();
-            VCardExample.ReadingAndWritingVCard(directoryPath);
-            //VCard40Example.SaveSingleVCardAsVcf(directoryPath);
+            Directory.Delete(directoryPath, true);
         }
+
+        _ = Directory.CreateDirectory(directoryPath);
+
+
+        //WhatsAppDemo1.IntegrateWhatsAppNumberUsingIMPP();
+        //WhatsAppDemo2.UsingTheWhatsAppType();
+        VCardExample.ReadingAndWritingVCard(directoryPath);
+        //VCard40Example.SaveSingleVCardAsVcf(directoryPath);
     }
 }
