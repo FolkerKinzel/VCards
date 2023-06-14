f0dd3bce39980bd140eedecb1b86c2ee565e9e01
Folker Kinzel <fkinzel@freenet.de>
2023-06-14 02:20:20 +0200

    Code formatting

--- src/FolkerKinzel.VCards/AnsiFilter.cs
+++ src/FolkerKinzel.VCards/AnsiFilter.cs
@@ -1,31 +1,30 @@
 ﻿using System.Text;
 using FolkerKinzel.Strings;
 
-namespace FolkerKinzel.VCards
+namespace FolkerKinzel.VCards;
+
+public class AnsiFilter
 {
-    public class AnsiFilter
+    private readonly UTF8Encoding _utf8 = new(false, true);
+    private readonly Encoding _ansi;
+
+    public AnsiFilter(int fallbackCodePage = 1252) => _ansi = TextEncodingConverter.GetEncoding(fallbackCodePage);
+
+    public AnsiFilter(string fallbackEncodingName) => _ansi = TextEncodingConverter.GetEncoding(fallbackEncodingName);
+
+    public Encoding FallbackEncoding => _ansi;
+
+    public bool LoadVcf(string fileName, out IList<VCard> vCards)
     {
-        private readonly UTF8Encoding _utf8 = new(false, true);
-        private readonly Encoding _ansi;
-
-        public AnsiFilter(int fallbackCodePage = 1252) => _ansi = TextEncodingConverter.GetEncoding(fallbackCodePage);
-
-        public AnsiFilter(string fallbackEncodingName) => _ansi = TextEncodingConverter.GetEncoding(fallbackEncodingName);
-
-        public Encoding FallbackEncoding => _ansi;
-
-        public bool LoadVcf(string fileName, out IList<VCard> vCards)
+        try
         {
-            try
-            {
-                vCards = VCard.LoadVcf(fileName, _utf8);
-                return false;
-            }
-            catch (DecoderFallbackException)
-            {
-                vCards = VCard.LoadVcf(fileName, _ansi);
-                return true;
-            }
+            vCards = VCard.LoadVcf(fileName, _utf8);
+            return false;
+        }
+        catch (DecoderFallbackException)
+        {
+            vCards = VCard.LoadVcf(fileName, _ansi);
+            return true;
         }
     }
 }
