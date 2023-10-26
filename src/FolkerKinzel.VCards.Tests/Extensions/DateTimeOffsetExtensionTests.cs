6eced884ea4dcc7ff6948a0bbe2d1ac6382d6298
Folker Kinzel <fkinzel@freenet.de>
2023-10-26 15:11:55 +0200

    tests

--- src/FolkerKinzel.VCards.Tests/Extensions/DateTimeOffsetExtensionTests.cs
+++ src/FolkerKinzel.VCards.Tests/Extensions/DateTimeOffsetExtensionTests.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FolkerKinzel.VCards.Intls.Converters;
+using FolkerKinzel.VCards.Intls.Deserializers;
 
 namespace FolkerKinzel.VCards.Extensions.Tests;
 
@@ -15,7 +16,28 @@ public class DateTimeOffsetExtensionTests
     [TestMethod()]
     public void HasYearTest1()
     {
-        
+        Assert.IsTrue(new DateTimeConverter().TryParse("--0304T170000".AsSpan(), out OneOf.OneOf<DateOnly, DateTimeOffset> oneOf));
+        Assert.IsTrue(oneOf.IsT1);
+        Assert.IsFalse(oneOf.AsT1.HasYear());
+        Assert.IsTrue(oneOf.AsT1.HasDate());
+    }
+
+    [TestMethod()]
+    public void HasYearTest2()
+    {
+        Assert.IsTrue(new DateTimeConverter().TryParse("16850304T170000".AsSpan(), out OneOf.OneOf<DateOnly, DateTimeOffset> oneOf));
+        Assert.IsTrue(oneOf.IsT1);
+        Assert.IsTrue(oneOf.AsT1.HasYear());
+        Assert.IsTrue(oneOf.AsT1.HasDate());
+    }
+
+    [TestMethod()]
+    public void HasYearTest3()
+    {
+        Assert.IsTrue(new TimeConverter().TryParse("T170000+02".AsSpan(), out OneOf.OneOf<TimeOnly, DateTimeOffset> oneOf));
+        Assert.IsTrue(oneOf.IsT1);
+        Assert.IsFalse(oneOf.AsT1.HasYear());
+        Assert.IsFalse(oneOf.AsT1.HasDate());
     }
 
     [TestMethod()]
