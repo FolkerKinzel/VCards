1e1d7867e193df9ca82d19189fbaad9189bb126c
Folker Kinzel <fkinzel@freenet.de>
2023-11-08 01:24:24 +0100

    tests

--- src/FolkerKinzel.VCards.Tests/Intls/Serializers/VcfSerializerTests.cs
+++ src/FolkerKinzel.VCards.Tests/Intls/Serializers/VcfSerializerTests.cs
@@ -1,4 +1,5 @@
 ﻿using FolkerKinzel.VCards.Intls.Serializers;
+using FolkerKinzel.VCards.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace FolkerKinzel.VCards.Tests.Intls.Serializers;
 
@@ -10,5 +11,32 @@ public class VcfSerializerTests
     public void GetSerializerTest1()
     {
         using var serializer = VcfSerializer.GetSerializer(new MemoryStream(), false, (VCdVersion)(-10000), VcfOptions.Default, null);
+    }
+
+    [TestMethod]
+    public void ResetBuildersTest1()
+    {
+        using var stream = new MemoryStream();
+        using var serializer = VcfSerializer.GetSerializer(stream, false, VCdVersion.V3_0, VcfOptions.Default, null);
+
+        serializer.Builder.Capacity = 20000;
+        serializer.Worker.Capacity = 20000;
+
+        var vc = new VCard { DisplayNames = new TextProperty("Goofy") };
+
+        serializer.Serialize(vc);
+
+        Assert.IsTrue(serializer.Builder.Capacity < 10000);
+        Assert.IsTrue(serializer.Worker.Capacity < 10000);
+    }
+
+    [TestMethod]
+    public void AppendLineFoldingTest1()
+    {
+        var vc = new VCard { DisplayNames = new TextProperty("A" + new string('ä', 80)) };
+
+        string s = vc.ToVcfString();
+
+        Assert.IsTrue(s.GetLinesCount() > 5);
     }
 }
