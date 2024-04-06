6375d84766a4ed91ad7d531ea675b62af2063bdf
Folker Kinzel <fkinzel@freenet.de>
2024-04-06 17:04:55 +0200

    example

---
+++ src/Examples/ExtensionMethodExample.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FolkerKinzel.VCards;
+
+namespace Examples;
+
+public  class ExtensionMethodExample
+{
+    public static void Example()
+    {
+        var vc = new VCard();
+    }
+}
