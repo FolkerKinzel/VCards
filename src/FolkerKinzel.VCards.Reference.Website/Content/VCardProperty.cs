72026663fb07f968d7bf126a0e522b415104721b
Folker Kinzel <fkinzel@freenet.de>
2024-07-13 17:19:57 +0200

    reference

---
+++ src/FolkerKinzel.VCards.Reference.Website/Content/VCardProperty.cs
@@ -0,0 +1,8 @@
+﻿public abstract class VCardProperty
+{
+    public string? Group { get; set; }
+
+    public ParameterSection Parameters { get; }
+
+    public virtual object? Value { get; protected set; }
+}
