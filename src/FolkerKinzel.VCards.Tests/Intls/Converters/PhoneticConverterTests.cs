b62d576037d15164afd718c093358197277d6b29
Folker Kinzel <fkinzel@freenet.de>
2024-07-24 01:14:10 +0200

    tests

---
+++ src/FolkerKinzel.VCards.Tests/Intls/Converters/PhoneticConverterTests.cs
@@ -0,0 +1,25 @@
+﻿using FolkerKinzel.VCards.Enums;
+
+namespace FolkerKinzel.VCards.Intls.Converters.Tests;
+
+[TestClass]
+public class PhoneticConverterTests
+{
+    [TestMethod]
+    public void Roundtrip()
+    {
+        foreach (Phonetic p in (Phonetic[])Enum.GetValues(typeof(Phonetic)))
+        {
+            Phonetic? p2 = PhoneticConverter.Parse(p.ToString().AsSpan());
+            Assert.AreEqual(p, p2);
+            object p3 = Enum.Parse(typeof(Phonetic), ((Phonetic?)p).ToVcfString() ?? "", true);
+            Assert.AreEqual(p, p3);
+        }
+
+        // Test auf nicht definiert
+        Assert.AreEqual(null, ((Phonetic?)4711).ToVcfString());
+    }
+
+    [TestMethod]
+    public void ParseTest() => Assert.IsNull(PhoneticConverter.Parse("nichtvorhanden".AsSpan()));
+}
