0808fa48a10d6d03cd7bb00362736238b7118586
Folker Kinzel <fkinzel@freenet.de>
2024-07-23 19:37:39 +0200

    reference

--- src/FolkerKinzel.VCards/Enums/Adr.cs
+++ src/FolkerKinzel.VCards/Enums/Adr.cs
@@ -16,19 +16,19 @@ public enum Adr
     // CAUTION: If the enum is expanded, AdrConverter and
     // EnumValueCollector must be adjusted!
 
-    /// <summary> <c>DOM</c>: Domestic delivery address (2,3,4)</summary>
+    /// <summary> <c>DOM</c>: Domestic delivery address (2,3)</summary>
     Dom = 1,
 
 
-    /// <summary> <c>INTL</c>: International delivery address (2,3,4)</summary>
+    /// <summary> <c>INTL</c>: International delivery address (2,3)</summary>
     Intl = 2,
 
 
-    /// <summary> <c>POSTAL</c>: Postal delivery address (2,3,4)</summary>
+    /// <summary> <c>POSTAL</c>: Postal delivery address (2,3)</summary>
     Postal = 4,
 
 
-    /// <summary> <c>PARCEL</c>: Parcel delivery address (2,3,4)</summary>
+    /// <summary> <c>PARCEL</c>: Parcel delivery address (2,3)</summary>
     Parcel = 8,
 
     /// <summary> <c>billing</c>: Billing address (4 - RFC 9554)</summary>
--- src/FolkerKinzel.VCards/Enums/Rel.cs
+++ src/FolkerKinzel.VCards/Enums/Rel.cs
@@ -15,78 +15,78 @@ public enum Rel
 {
     // CAUTION: If the enum is expanded, EnumValueCollector must be adjusted!
 
-    /// <summary> <c>CONTACT</c>: Someone you know how to get in touch with. Often symmetric.
+    /// <summary> <c>contact</c>: Someone you know how to get in touch with. Often symmetric.
     /// </summary>
     Contact = 1,
 
-    /// <summary> <c>ACQUAINTANCE</c>: Someone who you have exchanged greetings and
+    /// <summary> <c>acquaintance</c>: Someone who you have exchanged greetings and
     /// not much (if any) more — maybe a short conversation or two. Often symmetric.</summary>
     Acquaintance = 1 << 1,
 
-    /// <summary> <c>FRIEND</c>: Someone you are a friend to. A compatriot, buddy, home(boy|girl)
+    /// <summary> <c>friend</c>: Someone you are a friend to. A compatriot, buddy, home(boy|girl)
     /// that you know. Often symmetric.</summary>
     Friend = 1 << 2,
 
-    /// <summary> <c>MET</c>: Someone who you have actually met in person. Symmetric.</summary>
+    /// <summary> <c>met</c>: Someone who you have actually met in person. Symmetric.</summary>
     Met = 1 << 3,
 
-    /// <summary> <c>CO-WORKER</c>: Someone a person works with, or works at the same
+    /// <summary> <c>co-worker</c>: Someone a person works with, or works at the same
     /// organization as. Symmetric. Usually transitive.</summary>
     CoWorker = 1 << 4,
 
-    /// <summary> <c>COLLEAGUE</c>: Someone in the same field of study/activity. Symmetric.
+    /// <summary> <c>colleague</c>: Someone in the same field of study/activity. Symmetric.
     /// Often transitive.</summary>
     Colleague = 1 << 5,
 
-    /// <summary> <c>CO-RESIDENT</c>: Someone you share a street address with. Symmetric
+    /// <summary> <c>co-resident</c>: Someone you share a street address with. Symmetric
     /// and transitive.</summary>
     CoResident = 1 << 6,
 
-    /// <summary> <c>NEIGHBOR</c>: Someone who lives nearby, perhaps only at an adjacent
+    /// <summary> <c>neighbor</c>: Someone who lives nearby, perhaps only at an adjacent
     /// street address or doorway. Symmetric. Often transitive.</summary>
     Neighbor = 1 << 7,
 
-    /// <summary> <c>CHILD</c>: A person's genetic offspring, or someone that a person
+    /// <summary> <c>child</c>: A person's genetic offspring, or someone that a person
     /// has adopted and takes care of. Inverse is parent.</summary>
     Child = 1 << 8,
 
-    /// <summary> <c>PARENT</c>: Elternteil: Das Gegenteil von <see cref="Child" />.
+    /// <summary> <c>parent</c>: Elternteil: Das Gegenteil von <see cref="Child" />.
     /// </summary>
     Parent = 1 << 9,
 
-    /// <summary> <c>SIBLING</c>: Someone a person shares a parent with. Symmetric.
+    /// <summary> <c>sibling</c>: Someone a person shares a parent with. Symmetric.
     /// Usually transitive.</summary>
     Sibling = 1 << 10,
 
-    /// <summary> <c>SPOUSE</c>: Someone you are married to. Symmetric. Not transitive.</summary>
+    /// <summary> <c>spouse</c>: Someone you are married to. Symmetric. Not transitive.</summary>
     Spouse = 1 << 11,
 
-    /// <summary> <c>KIN</c>: A relative, someone you consider part of your extended
+    /// <summary> <c>kin</c>: A relative, someone you consider part of your extended
     /// family. Symmetric and typically transitive.</summary>
     Kin = 1 << 12,
 
-    /// <summary> <c>MUSE</c>: Someone who brings you inspiration. No inverse.</summary>
+    /// <summary> <c>muse</c>: Someone who brings you inspiration. No inverse.</summary>
     Muse = 1 << 13,
 
-    /// <summary> <c>CRUSH</c>: Someone you have a crush on. No inverse.</summary>
+    /// <summary> <c>crush</c>: Someone you have a crush on. No inverse.</summary>
     Crush = 1 << 14,
 
-    /// <summary> <c>DATE</c>: Someone you are dating. Symmetric. Not transitive.</summary>
+    /// <summary> <c>date</c>: Someone you are dating. Symmetric. Not transitive.</summary>
     Date = 1 << 15,
 
-    /// <summary> <c>SWEETHEART</c>: Someone with whom you are intimate and at least
+    /// <summary> <c>sweetheart</c>: Someone with whom you are intimate and at least
     /// somewhat committed, typically exclusively. Symmetric. Not transitive.</summary>
     Sweetheart = 1 << 16,
 
-    /// <summary> <c>ME</c>: A link to yourself at a different URL. Exclusive of all
+    /// <summary> <c>me</c>: A link to yourself at a different URL. Exclusive of all
     /// other XFN values. Required symmetric. There is an implicit "me" relation from
     /// the contents of a directory to the directory itself.</summary>
     Me = 1 << 17,
 
-    /// <summary> <c>AGENT</c>: An entity who may sometimes act on behalf of the entity
+    /// <summary> <c>agent</c>: An entity who may sometimes act on behalf of the entity
     /// associated with the vCard.</summary>
     Agent = 1 << 18,
 
-    /// <summary> <c>EMERGENCY</c>: Indicates an emergency contact.</summary>
+    /// <summary> <c>emergency</c>: Indicates an emergency contact.</summary>
     Emergency = 1 << 19
 }
