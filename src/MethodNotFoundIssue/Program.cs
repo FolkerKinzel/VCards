1e1a4fed5dcf75b59ca459089c2e426d5d4bdaf4
Folker Kinzel <fkinzel@freenet.de>
2024-10-06 01:24:29 +0200

    test

---
+++ src/MethodNotFoundIssue/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using FolkerKinzel.VCards;
+
+string vcf = new StringBuilder()
+   .AppendLine("BEGIN:VCARD")
+   .AppendLine("VERSION:3.0")
+   .AppendLine("FN:Folker")
+   .AppendLine("END:VCARD")
+   .ToString();
+
+using TextReader stringReader = new StringReader(vcf);
+using var vcfReader = new VcfReader(stringReader);
+
+IEnumerable<FolkerKinzel.VCards.VCard> result = vcfReader.ReadToEnd();
+
+var parsedVCard = result.First();
+
+Console.WriteLine(parsedVCard);
