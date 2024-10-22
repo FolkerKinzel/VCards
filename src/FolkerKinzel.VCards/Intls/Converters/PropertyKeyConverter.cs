19bf8bd4ba6ca98cafd243b11bceba58d7505056
Folker Kinzel <fkinzel@freenet.de>
2024-10-22 22:26:55 +0200

    PropertyKeyConverter

--- src/FolkerKinzel.VCards/Intls/Converters/PropertyKeyConverter.cs
+++ src/FolkerKinzel.VCards/Intls/Converters/PropertyKeyConverter.cs
@@ -15,6 +15,7 @@ internal static class PropertyKeyConverter
             : span.Equals(VCard.PropKeys.N, comp) ? VCard.PropKeys.N
             : span.Equals(VCard.PropKeys.ADR, comp) ? VCard.PropKeys.ADR
             : span.Equals(VCard.PropKeys.CLIENTPIDMAP, comp) ? VCard.PropKeys.CLIENTPIDMAP
+            : span.Equals(VCard.PropKeys.NonStandard.X_AB_LABEL, comp) ? VCard.PropKeys.NonStandard.X_AB_LABEL
             : span.Equals(VCard.PropKeys.URL, comp) ? VCard.PropKeys.URL
             : span.Equals(VCard.PropKeys.BDAY, comp) ? VCard.PropKeys.BDAY
             : span.Equals(VCard.PropKeys.ANNIVERSARY, comp) ? VCard.PropKeys.ANNIVERSARY
@@ -26,6 +27,7 @@ internal static class PropertyKeyConverter
             : span.Equals(VCard.PropKeys.Rfc9554.GRAMGENDER, comp) ? VCard.PropKeys.Rfc9554.GRAMGENDER
             : span.Equals(VCard.PropKeys.Rfc9554.SOCIALPROFILE, comp) ? VCard.PropKeys.Rfc9554.SOCIALPROFILE
             : span.Equals(VCard.PropKeys.NonStandard.X_SOCIALPROFILE, comp) ? VCard.PropKeys.Rfc9554.SOCIALPROFILE
+            : span.Equals(VCard.PropKeys.Rfc9555.JSPROP, comp) ? VCard.PropKeys.Rfc9555.JSPROP
             : span.ToString().ToUpperInvariant();
     }
 }
