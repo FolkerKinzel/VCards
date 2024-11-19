90b2568e1686ac7cfc10bbb58269c5ead0e59e44
Folker Kinzel <fkinzel@freenet.de>
2024-11-19 21:49:43 +0100

    tests

--- src/Examples/VCardExample.cs
+++ src/Examples/VCardExample.cs
@@ -190,8 +190,8 @@ VCard2.vcf:
 ----------
 BEGIN:VCARD
 VERSION:2.1
-REV:2024-10-12T14:01:32Z
-UID:3fbf66bd-ca03-4445-8916-e7ab837a4cee
+REV:2024-11-19T20:37:56Z
+UID:01934625-6308-7541-ad4e-070878d7f286
 0.FN;ENCODING=QUOTED-PRINTABLE;CHARSET=UTF-8;LANGUAGE=de-DE:Prof. Dr. K=C3=
 =A4the Alexandra Caroline M=C3=BCller-Risinowsky II.
 0.N;ENCODING=QUOTED-PRINTABLE;CHARSET=UTF-8;LANGUAGE=de-DE:M=C3=BCller-Ris=
@@ -202,9 +202,9 @@ ORG:Millers Company;C#;Webdesign
 BDAY:1984-03-28
 X-ANNIVERSARY:2006-07-14
 1.ADR;DOM;INTL;POSTAL;PARCEL;WORK;ENCODING=QUOTED-PRINTABLE;CHARSET=UTF-8:=
-;;22 Friedrichstra=C3=9Fe;Berlin;;10117;Germany
+;;Friedrichstra=C3=9Fe 22;Berlin;;10117;Germany
 1.LABEL;DOM;INTL;POSTAL;PARCEL;WORK;ENCODING=QUOTED-PRINTABLE;CHARSET=UTF-8:=
-Friedrichstra=C3=9Fe 22=0D=0A10117 Berlin=0D=0AGERMANY
+Friedrichstra=C3=9Fe 22=0D=0A10117 Berlin=0D=0AGermany
 1.GEO:52.511821;13.389581
 1.TZ:+01:00
 TEL;WORK;VOICE;MSG;CELL;BBS:tel:+49-321-1234567
@@ -216,8 +216,8 @@ X-SPOUSE;ENCODING=QUOTED-PRINTABLE;CHARSET=UTF-8:Paul M=C3=BCller-Risinows=
 ky
 NOTE:Very experienced in Blazor.
 PHOTO;ENCODING=BASE64;TYPE=JPEG:
- knlPSqqVzaKYiddMZGoqJ7NUnjVklXwbPBoXLRgno32vyBW2dHU9sVpvnEiiAx7BDe+XpLhGE2
- p8n5bH
+ bi6Y0Ny3abtApQ6qMsWxkiAs5TpMDO5Rug2bMp+nmgLBVtDuvsSxeZOW0jx17jp6yn/f76dqmX
+ 4yp1Ah
 
 END:VCARD
 
@@ -226,8 +226,8 @@ VCard3.vcf:
 ----------
 BEGIN:VCARD
 VERSION:3.0
-REV:2024-10-12T14:01:32Z
-UID:3fbf66bd-ca03-4445-8916-e7ab837a4cee
+REV:2024-11-19T20:37:56Z
+UID:01934625-6308-7541-ad4e-070878d7f286
 0.FN;LANGUAGE=de-DE:Prof. Dr. Käthe Alexandra Caroline Müller-Risinowsky
  II.
 0.N;LANGUAGE=de-DE:Müller-Risinowsky;Käthe;Alexandra Caroline;Prof. Dr.;I
@@ -237,10 +237,10 @@ TITLE:CEO
 ORG:Millers Company;C#;Webdesign
 BDAY;VALUE=DATE:1984-03-28
 X-ANNIVERSARY:2006-07-14
-1.ADR;TYPE=WORK,DOM,INTL,POSTAL,PARCEL:;;22 Friedrichstraße;Berlin;;10117;
+1.ADR;TYPE=WORK,DOM,INTL,POSTAL,PARCEL:;;Friedrichstraße 22;Berlin;;10117;
  Germany
 1.LABEL;TYPE=WORK,DOM,INTL,POSTAL,PARCEL:Friedrichstraße 22\n10117 Berlin\
- nGERMANY
+ nGermany
 1.GEO:52.511821;13.389581
 1.TZ:+01:00
 TEL;TYPE=WORK,VOICE,MSG,CELL,BBS:tel:+49-321-1234567
@@ -251,8 +251,8 @@ X-SOCIALPROFILE;X-SERVICE-TYPE=Y:https://y.com/Semaphore
 IMPP;X-SERVICE-TYPE=WhatsDown:https://wd.me/0123456789
 X-SPOUSE:Paul Müller-Risinowsky
 NOTE:Very experienced in Blazor.
-PHOTO;ENCODING=b;TYPE=JPEG:knlPSqqVzaKYiddMZGoqJ7NUnjVklXwbPBoXLRgno32vyBW2
- dHU9sVpvnEiiAx7BDe+XpLhGE2p8n5bH
+PHOTO;ENCODING=b;TYPE=JPEG:bi6Y0Ny3abtApQ6qMsWxkiAs5TpMDO5Rug2bMp+nmgLBVtDu
+ vsSxeZOW0jx17jp6yn/f76dqmX4yp1Ah
 END:VCARD
 
 
@@ -260,9 +260,9 @@ VCard4.vcf:
 ----------
 BEGIN:VCARD
 VERSION:4.0
-CREATED;VALUE=TIMESTAMP:20241012T140132Z
-REV:20241012T140132Z
-UID:urn:uuid:3fbf66bd-ca03-4445-8916-e7ab837a4cee
+CREATED;VALUE=TIMESTAMP:20241119T203755Z
+REV:20241119T203756Z
+UID:urn:uuid:01934625-6308-7541-ad4e-070878d7f286
 0.FN;DERIVED=TRUE;LANGUAGE=de-DE;PID=1.1:Prof. Dr. Käthe Alexandra Carolin
  e Müller-Risinowsky II.
 0.N;LANGUAGE=de-DE;PID=1.1:Müller-Risinowsky;Käthe;Alexandra,Caroline;Pro
@@ -273,8 +273,8 @@ ORG;PID=1.1:Millers Company;C#;Webdesign
 BDAY;VALUE=DATE;PID=1.1:19840328
 ANNIVERSARY;VALUE=DATE;PID=1.1:20060714
 1.ADR;TYPE=WORK,billing,delivery;LABEL=Friedrichstraße 22\n10117 Berlin\nG
- ERMANY;GEO="geo:52.511821,13.389581";TZ=Europe/Berlin;PID=1.1:;;22,Friedri
- chstraße;Berlin;;10117;Germany;;;;22;Friedrichstraße;;;;;;
+ ermany;GEO="geo:52.511821,13.389581;u=10";TZ=Europe/Berlin;PID=1.1:;;Fried
+ richstraße,22;Berlin;;10117;Germany;;;;22;Friedrichstraße;;;;;;
 TEL;TYPE=WORK,VOICE,CELL,TEXT;VALUE=URI;PID=1.1;INDEX=1:tel:+49-321-1234567
 TEL;TYPE=HOME,VOICE;VALUE=URI;PID=2.1;INDEX=2:tel:+49-123-9876543
 EMAIL;TYPE=WORK;PREF=1;PID=1.1:kaethe_mueller@internet.com
@@ -285,10 +285,11 @@ IMPP;SERVICE-TYPE=WhatsDown;PID=1.1:https://wd.me/0123456789
 RELATED;TYPE=colleague,co-resident,spouse;VALUE=TEXT;PID=1.1:Paul Müller-R
  isinowsky
 NOTE;PID=1.1;AUTHOR-NAME=Satya Nadella;AUTHOR="https://www.microsoft.com/";
- CREATED=20241012T140132Z:Very experienced in Blazor.
-PHOTO;PID=1.1:data:image/jpeg;base64,knlPSqqVzaKYiddMZGoqJ7NUnjVklXwbPBoXLR
- gno32vyBW2dHU9sVpvnEiiAx7BDe+XpLhGE2p8n5bH
+ CREATED=20241119T203756Z:Very experienced in Blazor.
+PHOTO;PID=1.1:data:image/jpeg;base64,bi6Y0Ny3abtApQ6qMsWxkiAs5TpMDO5Rug2bMp
+ +nmgLBVtDuvsSxeZOW0jx17jp6yn/f76dqmX4yp1Ah
 CLIENTPIDMAP:1;urn:uuid:53e374d9-337e-4727-8803-a1e9c14e0556
+GRAMGENDER;LANGUAGE=de;PID=1.1:feminine
 END:VCARD
 
 
@@ -297,17 +298,17 @@ Read VCard:
 Version: 3.0
 
 [DataType: TimeStamp]
-TimeStamp: 10/12/2024 14:01:32 +00:00
+Updated: 11/19/2024 20:37:56 +00:00
 
-ID: 3fbf66bd-ca03-4445-8916-e7ab837a4cee
+ContactID: System.Guid: 01934625-6308-7541-ad4e-070878d7f286
 
 GenderViews: Female
 
 Titles: CEO
 
 Organizations:
-    OrganizationName:    Millers Company
-    OrganizationalUnits: C#; Webdesign
+    Name:  Millers Company
+    Units: C#; Webdesign
 
 [DataType: Date]
 BirthDayViews: System.DateOnly: 03/28/1984
@@ -337,7 +338,7 @@ Messengers: https://wd.me/0123456789
 
 [RelationType: Spouse]
 [DataType: Text]
-Relations: System.String: Paul Müller-Risinowsky
+Relations: FolkerKinzel.VCards.Models.ContactID: System.String: Paul Müller-Risinowsky
 
 Notes: Very experienced in Blazor.
 
@@ -352,11 +353,11 @@ DisplayNames: Prof. Dr. Käthe Alexandra Caroline Müller-Risinowsky II.
 [Language: de-DE]
 [Group: 0]
 NameViews:
-    FamilyNames:     Müller-Risinowsky
-    GivenNames:      Käthe
-    AdditionalNames: Alexandra Caroline
-    Prefixes:        Prof. Dr.
-    Suffixes:        II.
+    Surnames: Müller-Risinowsky
+    Given:    Käthe
+    Given2:   Alexandra Caroline
+    Prefixes: Prof. Dr.
+    Suffixes: II.
 
 [Group: 1]
 TimeZones: +01:00
@@ -371,14 +372,14 @@ GeoCoordinates:
 [Label:
     Friedrichstraße 22
     10117 Berlin
-    GERMANY]
+    Germany]
 [GeoPosition:
     Latitude:    52.511821
     Longitude:   13.389581]
 [TimeZone: +01:00]
 [Group: 1]
 Addresses:
-    Street:     22 Friedrichstraße
+    Street:     Friedrichstraße 22
     Locality:   Berlin
     PostalCode: 10117
     Country:    Germany
