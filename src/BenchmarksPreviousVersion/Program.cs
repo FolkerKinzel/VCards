017247d9c37373e75735307724c94c3d55a7ac5e
Folker Kinzel <fkinzel@freenet.de>
2024-12-03 00:53:57 +0100

    remove BenchmarksPreviousVersion

--- src/BenchmarksPreviousVersion/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using BenchmarkDotNet.Running;
-using Benchmarks;
-
-//_ = BenchmarkRunner.Run<ParseBench>();
-_ = BenchmarkRunner.Run<WriteBench>();
