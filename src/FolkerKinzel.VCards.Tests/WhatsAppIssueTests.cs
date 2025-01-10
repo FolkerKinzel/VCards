4f17d21f5f8eef7420200995500583083f4c234b
Folker Kinzel <fkinzel@freenet.de>
2025-01-10 03:44:38 +0100

    test

--- src/FolkerKinzel.VCards.Tests/WhatsAppIssueTests.cs
+++ src/FolkerKinzel.VCards.Tests/WhatsAppIssueTests.cs
@@ -1,4 +1,5 @@
-﻿using FolkerKinzel.VCards.Models.Properties;
+﻿using FolkerKinzel.VCards.Models;
+using FolkerKinzel.VCards.Models.Properties;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace FolkerKinzel.VCards.Tests;
