99f3992e5290439ecd80b8c945a2222b47a96628
Folker Kinzel <fkinzel@freenet.de>
2026-02-13 02:12:33 +0100

    fix the issue

--- src/Net472Issue/Program.cs
+++ src/Net472Issue/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FolkerKinzel.VCards;
+using FolkerKinzel.VCards.Enums;
+using FolkerKinzel.VCards.Extensions;
 
 namespace Net472Issue
 {
@@ -12,14 +15,18 @@ namespace Net472Issue
         static void Main(string[] args)
         {
             var nb = NameBuilder.Create();
-            nb.AddGiven("Susi")
-              .AddSurname("Sonntag");
+            nb.AddGiven("Jürgen")
+              .AddSurname("Müller");
+
 
             var builder = VCardBuilder.Create(true, true);
 
             builder.NameViews.Add(nb.Build());
+            builder.NameViews.ToDisplayNames(NameFormatter.Default);
 
-            Console.WriteLine(builder.VCard.NameViews.First().Value.Given[0]);
+            Console.WriteLine(builder.VCard.DisplayNames.First().Value);
+
+            //File.WriteAllText("vCard3_0.vcf", Vcf.AsString(builder.VCard), Encoding.GetEncoding(1252));
         }
     }
 }
